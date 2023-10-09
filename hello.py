786287304df0c30b8ad1e54a720235e1503056bc
Wee Li <91833516+Nateliew@users.noreply.github.com>
2023-10-09 23:48:42 +0800

    helloPython

---
+++ hello.py
@@ -0,0 +1,36 @@
+import random
+msg = "Welcome to my first game!"
+print(msg)
+
+name = input("What is your name? ")
+age= int(input("What is your age? "))
+print("Hello", name)
+
+
+if age>=18:
+    print("You can play")
+    wants_to_play = input("Do you want to play? Type yes or no: ").lower()
+    if wants_to_play == "yes":
+        print("Yay!")
+
+        randomNum = random.randint(0,9)
+        
+        guess = int(input("Guess a number between 0 and 9. You have 5 chances. "))
+        i = 1
+        while i < 5:
+             
+            if guess == randomNum:
+                    print("You won!")
+                    break
+            else: 
+                int(input("You have " + str(5-i)  + " chances. Guess again: "))
+                i +=1
+
+    else: 
+        print("Bye!")
+
+
+# elif age >=13:
+#     print("You need adult supervision")
+else:
+    print("You are not qualified.")
